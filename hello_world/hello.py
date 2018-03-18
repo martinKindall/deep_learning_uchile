4b088e3d33738fa35231944c043da510ae87aa7d
martinKindall <martin@rflex.cl>
2018-03-18 17:10:50 -0300

    testing basic code in python

---
+++ hello_world/hello.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+
+for n in range(10):
+	print(n)
+
+def square(number):
+	return number*number;
+
+number = input("Enter a number: ")
+
+print(str(number)+" squared is "+str(square(number)))
