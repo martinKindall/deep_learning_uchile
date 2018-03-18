06825165aace651be7cc29a1d72a8d0cd7af224a
martinKindall <martin@rflex.cl>
2018-03-18 18:13:19 -0300

    creando tensores aleatorios

--- hello_world/funciones_activacion_test.py
+++ hello_world/funciones_activacion_test.py
@@ -9,4 +9,13 @@ def relu(T):
 print(str(relu(10)))
 print(str(relu(-10)))
 
-print(torch.exp(3))
+# print(torch.exp(3))
+
+print(torch.is_tensor(3))
+
+a = torch.randn(3,3,3)
+# [torch.FloatTensor of size 3x3x3]
+print(a)
+print(torch.is_tensor(a))
+
+
