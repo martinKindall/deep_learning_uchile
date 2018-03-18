533e3815aa801a8d2a5d9595c40a06618f8bf91d
martinKindall <martin@rflex.cl>
2018-03-18 18:37:50 -0300

    Se implementa relu para tensores usando operaciones de tensores

--- hello_world/funciones_activacion_test.py
+++ hello_world/funciones_activacion_test.py
@@ -1,21 +1,38 @@
 import torch
 
 def relu(T):
-	if T<=0:
-		return 0
+	S = torch.abs(T)
+	return torch.div(torch.add(T, 1, S), 2)
 
-	return T
+def main1():
 
-print(str(relu(10)))
-print(str(relu(-10)))
+	print(str(relu(10)))
+	print(str(relu(-10)))
 
-# print(torch.exp(3))
+	# print(torch.exp(3))
 
-print(torch.is_tensor(3))
+	# print(torch.is_tensor(3))
 
-a = torch.randn(3,3,3)
-# [torch.FloatTensor of size 3x3x3]
-print(a)
-print(torch.is_tensor(a))
+	print("Input tensor: \n")
+	a = torch.randn(3,3,3)
+	print(a)
+	# [torch.FloatTensor of size 3x3x3]
+	# print(torch.is_tensor(a))
+	# True
+	# print(torch.numel(a))
+	# 27
+	b = torch.abs(a)
 
+	print("Output relu tensor: \n")
+	relu = torch.div(torch.add(a, 1, b), 2)
+	print(relu)
 
+def main2():
+	a = torch.randn(3,3,3)
+
+	print("Input tensor: \n")
+	print(a)
+	print("Relu'd tensor: \n")
+	print(relu(a))
+
+main2()
