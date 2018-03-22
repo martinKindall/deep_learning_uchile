374fe0dd11d5153b0d756af197b7b954d084588e
martinKindall <martin@rflex.cl>
2018-03-21 21:37:30 -0300

    Parte 2a) Se inicializan los parametros para una red de 1 capa escondida

--- hello_world/FFNN.py
+++ hello_world/FFNN.py
@@ -1,4 +1,5 @@
 import pdb
+import torch
 
 class FFNN():
 	def __init__(self, F, l_h, l_a, C):
@@ -7,5 +8,10 @@ class FFNN():
 		self.l_a = l_a
 		self.C = C
 
-red_neuronal = FFNN(10, [10], ['algo'], 2)
+		self.W_1 = torch.randn(F, l_h)
+		self.b_1 = torch.zeros(1, l_h)
+		self.U = torch.randn(l_h, C)
+		self.c_init = torch.zeros(1, C)
+
+red_neuronal = FFNN(10, 10, ['algo'], 2)
 pdb.set_trace()
