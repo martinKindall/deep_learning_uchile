d9af71dbea5e87675efa48c669e3c7c5289644b9
martinKindall <martin@rflex.cl>
2018-03-21 22:10:05 -0300

    Tarea 1 2b) se crean funciones para pasar parametros a GPU o CPU

--- hello_world/FFNN.py
+++ hello_world/FFNN.py
@@ -8,10 +8,24 @@ class FFNN():
 		self.l_a = l_a
 		self.C = C
 
+		self.parametros = []
 		self.W_1 = torch.randn(F, l_h)
 		self.b_1 = torch.zeros(1, l_h)
 		self.U = torch.randn(l_h, C)
 		self.c_init = torch.zeros(1, C)
+
+	def gpu(self):
+	    if torch.cuda.is_available():
+		    self.W_1 = self.W_1.cuda()
+		    self.b_1 = self.b_1.cuda()
+		    self.U = self.U.cuda()
+		    self.c_init = self.c_init.cuda()
+  
+	def cpu(self):
+	    self.W_1 = self.W_1.cpu()
+	    self.b_1 = self.b_1.cpu()
+	    self.U = self.U.cpu()
+	    self.c_init = self.c_init.cpu()
 
 red_neuronal = FFNN(10, 10, ['algo'], 2)
 pdb.set_trace()
