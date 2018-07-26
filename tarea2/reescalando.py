f2110981bce6010cd7bd67c0f5b3c23e3a4fe633
martinKindall <martin@rflex.cl>
2018-07-25 22:04:54 -0400

    Se agregan ejemplos de dibujos mal hechos o poco claros
    Se termina tarea 2

--- tarea2/reescalando.py
+++ tarea2/reescalando.py
@@ -2,15 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.misc import imresize
 
-orig = np.loadtxt("letra_64_64.txt")
+orig = np.loadtxt("escoba_train_mal2.txt")
 
-resize = imresize(orig, (128,128))
+# resize = imresize(orig, (128,128))
 
-relu = lambda x: 1 if x > 0 else 0
-vfunc = np.vectorize(relu)
-resize = vfunc(resize)
+# relu = lambda x: 1 if x > 0 else 0
+# vfunc = np.vectorize(relu)
+# resize = vfunc(resize)
 
-np.savetxt("letra_res.txt", resize, fmt="%d", delimiter=" ")
+# np.savetxt("escoba_train.txt", resize, fmt="%d", delimiter=" ")
 
-plt.imshow(orig)
-plt.show()
+plt.imshow(orig, cmap='Greys')
+plt.savefig('escoba_train_mal.png')
